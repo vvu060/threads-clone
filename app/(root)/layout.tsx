10e700ec675582dc2e7727bf0b69ab82e8734734
vvu060 <vvu060@gmail.com>
2023-08-25 09:25:52 +0530

    create layout components for authenticated pages

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -5,6 +5,10 @@ import { ClerkProvider } from '@clerk/nextjs';
 import { dark } from '@clerk/themes';
 
 import '../globals.css';
+import Topbar from '@/components/shared/Topbar';
+import LeftSidebar from '@/components/shared/LeftSidebar';
+import RightSidebar from '@/components/shared/RightSidebar';
+import Bottombar from '@/components/shared/Bottombar';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -25,7 +29,19 @@ export default function RootLayout({
       }}
     >
       <html lang='en'>
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <Topbar />
+          <main>
+            <LeftSidebar />
+
+            <section className='main-container'>
+              <div className='w-full max-w-4xl'>{children}</div>
+            </section>
+            <RightSidebar />
+          </main>
+          {children}
+          <Bottombar />
+        </body>
       </html>
     </ClerkProvider>
   );
