fbfe2d6c9ad6c392d25df5484b6bd8f9a73a2b5c
vvu060 <vvu060@gmail.com>
2023-08-26 11:40:29 +0530

    thread details page with comments

---
+++ components/forms/Comment.tsx
@@ -0,0 +1,86 @@
+'use client';
+
+import React from 'react';
+import * as z from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Button } from '../ui/button';
+import { Input } from '../ui/input';
+import { usePathname, useRouter } from 'next/navigation';
+import Image from 'next/image';
+import { addCommentToThread } from '@/lib/actions/thread.actions';
+import { CommentValidation } from '@/lib/validations/thread';
+
+interface CommentProps {
+  threadId: string;
+  currentUserImg: string;
+  currentUserId: string;
+}
+
+const Comment = ({ threadId, currentUserImg, currentUserId }: CommentProps) => {
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const form = useForm({
+    resolver: zodResolver(CommentValidation),
+    defaultValues: {
+      thread: '',
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof CommentValidation>) => {
+    await addCommentToThread(
+      threadId,
+      values.thread,
+      JSON.parse(currentUserId),
+      pathname
+    );
+
+    form.reset();
+  };
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className='comment-form'>
+        <FormField
+          control={form.control}
+          name='thread'
+          render={({ field }) => (
+            <FormItem className='flex items-center gap-3 w-full'>
+              <FormLabel>
+                <Image
+                  src={currentUserImg}
+                  alt='profile image'
+                  height={48}
+                  width={48}
+                  className='rounded-full cursor-pointer'
+                />
+              </FormLabel>
+              <FormControl className='border-none bg-transparent'>
+                <Input
+                  type='text'
+                  placeholder='Comment...'
+                  className='n-focus text-light-1'
+                  {...field}
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <Button type='submit' className='comment-form_btn'>
+          Reply
+        </Button>
+      </form>
+    </Form>
+  );
+};
+
+export default Comment;
