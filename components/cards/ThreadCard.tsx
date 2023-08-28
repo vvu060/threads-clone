d1d08684c83d3d178746dce116705afce363b254
vvu060 <vvu060@gmail.com>
2023-08-28 17:51:55 +0530

    use organization if it is posted as org

--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -1,3 +1,4 @@
+import { formatDateString } from '@/lib/utils';
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
@@ -112,6 +113,26 @@ const ThreadCard = ({
           </div>
         </div>
       </div>
+      {/* TODO: delete thread */}
+      {/* TODO: show comment logos */}
+      {!isComment && community && (
+        <Link
+          href={`/communities/${community.id}`}
+          className='mt-5 flex items-center'
+        >
+          <p className='text-subtle-medium text-gray-1'>
+            {formatDateString(createdAt)} - {community.name} Community
+          </p>
+
+          <Image
+            src={community.image}
+            alt={community.name}
+            height={14}
+            width={14}
+            className='ml-1 rounded-full object-cover'
+          />
+        </Link>
+      )}
     </article>
   );
 };
--- components/forms/PostThread.tsx
+++ components/forms/PostThread.tsx
@@ -18,10 +18,12 @@ import { usePathname, useRouter } from 'next/navigation';
 import { updateUser } from '@/lib/actions/user.actions';
 import { ThreadValidation } from '@/lib/validations/thread';
 import { createThread } from '@/lib/actions/thread.actions';
+import { useOrganization } from '@clerk/nextjs';
 
 const PostThread = ({ userId }: { userId: string }) => {
   const router = useRouter();
   const pathname = usePathname();
+  const { organization } = useOrganization();
 
   const form = useForm({
     resolver: zodResolver(ThreadValidation),
@@ -35,7 +37,7 @@ const PostThread = ({ userId }: { userId: string }) => {
     await createThread({
       text: values.thread,
       author: userId,
-      communityId: null,
+      communityId: organization ? organization.id : null,
       path: pathname,
     });
 
