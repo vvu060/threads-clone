8afb7315d762ac2c5256f9891d96ace4237446a2
vvu060 <vvu060@gmail.com>
2023-08-28 19:05:47 +0530

    add community details UI comps

--- components/shared/ThreadsTab.tsx
+++ components/shared/ThreadsTab.tsx
@@ -2,6 +2,7 @@ import { fetchUserPosts } from '@/lib/actions/user.actions';
 import { redirect } from 'next/navigation';
 import React from 'react';
 import ThreadCard from '../cards/ThreadCard';
+import { fetchCommunityPosts } from '@/lib/actions/community.actions';
 
 interface ThreadsTabProps {
   currentUserId: string;
@@ -14,7 +15,13 @@ const ThreadsTab = async ({
   accountId,
   accountType,
 }: ThreadsTabProps) => {
-  let result = await fetchUserPosts(accountId);
+  let result: any;
+
+  if (accountType === 'Community') {
+    result = await fetchCommunityPosts(accountId);
+  } else {
+    result = await fetchUserPosts(accountId);
+  }
 
   if (!result) redirect('/');
 
