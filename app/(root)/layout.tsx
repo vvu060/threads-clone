5df38a308fcf9eb83ceeaa84e318364ec3bd422e
vvu060 <vvu060@gmail.com>
2023-08-28 17:03:17 +0530

    fix error

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -40,7 +40,6 @@ export default function RootLayout({
             </section>
             <RightSidebar />
           </main>
-          {children}
           <Bottombar />
         </body>
       </html>
